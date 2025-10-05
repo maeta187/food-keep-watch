dfc81b81b41abd3d515688663446e837cba0d3cf
maeta187 <nexus18789@gmail.com>
2025-10-05 19:06:40 +0900

    🎨 Prettier 設定に合わせてコードを整形する

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,9 @@ import { Stack } from 'expo-router/stack'
 import '../global.css'
 
 export default function Layout() {
-  return (
-    <Stack>
-      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
-    </Stack>
-  )
+	return (
+		<Stack>
+			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+		</Stack>
+	)
 }
