52ba4ebecc47c79a5259def74e52844ec90cfe12
maeta187 <nexus18789@gmail.com>
2025-10-05 11:47:32 +0900

    🎉 : first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Tab() {
+  return (
+    <View className='bg-red-500 flex-1 justify-center items-center'>
+      <Text>Home</Text>
+    </View>
+  )
+}
