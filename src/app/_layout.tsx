4978824b621cc02a097f1c3006c499c4b1cdc404
maeta187 <nexus18789@gmail.com>
2025-11-30 11:05:05 +0900

    ➕: expo-notificationsを導入し通知ハンドラを初期化する

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from 'expo-router/stack'
+import { useEffect } from 'react'
 import '../global.css'
 
+import { setupNotificationHandler } from '@/src/features/notifications/setup-notification-handler'
+
 export default function Layout() {
+	useEffect(() => {
+		setupNotificationHandler()
+	}, [])
+
 	return (
 		<Stack>
 			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
